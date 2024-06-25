eaa4120184c545e65a32ca0099c9abd27a47fe99
chonny <hitsugiya@gmail.com>
2024-06-25 14:32:49 -0400

    create function to populate page with numxnum square divs

---
+++ script.js
@@ -0,0 +1,18 @@
+const gridContainer = document.querySelector(".container");
+
+function populateGrid(num) {
+    let totalSquares = num * num;
+    let percentOfRow = 100 / num;
+    for (let i = 0; i <= totalSquares - 1; i++) {
+        let square = document.createElement("div");
+        square.setAttribute("class","square");
+        square.style.cssText = `flex: 1 0 calc(${percentOfRow}% - 10px);`
+        const squareContent = document.createElement("div");
+        squareContent.setAttribute("class", "content")
+        gridContainer.appendChild(square)
+        square.appendChild(squareContent)
+    }
+    return null;
+}
+
+populateGrid(16);
